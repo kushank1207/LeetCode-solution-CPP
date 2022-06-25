40d48a21f519db56df9ef11696c4b769b3af8f26
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2022-06-26 01:09:00 +0530

    Time: 3 ms, Memory: 10.2 MB - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -8,13 +8,14 @@ public:
         
         while(i<=j) {
             int mid = i + (j-i)/2;
-            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid+1]) 
+            if(nums[mid] > nums[(mid+1)%N] && nums[mid] > nums[(mid+N-1)%N]) 
                 return nums[mid+1];
             else if(nums[mid] > nums[N-1])
-                i = mid+1;
-            else 
+                i = mid+1;  
+            else
                 j = mid-1;
         }
+        
         return -1;
     }
 };
