21a0e020c7c363ee23210220fd997f440d10e8ae
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2022-07-11 00:29:16 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.cpp
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -42,9 +42,7 @@ class Solution{
                 mn = min(mn, abs(range-2*i));
             }
         }
-        // for(int i=0; i<row.size(); i++) {
-            
-        // }
+
         return mn;
 	} 
 };
