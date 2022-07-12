7f6528d31d99202d18881a9452b3b729002eded7
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2022-07-12 22:22:08 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -13,10 +13,7 @@ class Solution{
     int cutRod(int price[], int n) {
         //code here
         int dp[n+1][n+1];
-        // int length[n];
-        // for(int i=0; i<n; i++) {
-        //     length[i] = i+1;
-        // }
+ 
         for(int i=0; i<n+1; i++) {
             for(int j=0; j<n+1; j++) {
                 if(i==0 || j==0) {
