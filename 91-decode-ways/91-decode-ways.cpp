48ee48a7be440009344d0c88bebce56672448cf1
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2022-10-07 22:56:38 -0400

    Time: 0 ms (100.00%), Space: 6.3 MB (46.11%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int dp[100];
+//     int dp[100];
     
-    int solve(string s, int i) {
-        if(s[i] == '0')
-            return 0;
-        if(i >= s.length())
-            return 1;
-        if(dp[i] != 0)
-            return dp[i];
+//     int solve(string s, int i) {
+//         if(s[i] == '0')
+//             return 0;
+//         if(i >= s.length())
+//             return 1;
+//         if(dp[i] != 0)
+//             return dp[i];
         
-        int res = solve(s, i+1);
-        if(i+1 < s.length() && (s[i] == '1' || (s[i] == '2' && s[i+1]-'0' >=0 && s[i+1]-'0' < 7))) {
-            res += solve(s, i+2);
-        }
-        return dp[i] = res;
-    }
+//         int res = solve(s, i+1);
+//         if(i+1 < s.length() && (s[i] == '1' || (s[i] == '2' && s[i+1]-'0' >=0 && s[i+1]-'0' < 7))) {
+//             res += solve(s, i+2);
+//         }
+//         return dp[i] = res;
+//     }
     
     int numDecodings(string s) {
-        memset(dp, 0, sizeof(dp));
-        // if(s[0] == '0')
-        //     return 0;
-        return solve(s, 0);
+        // memset(dp, 0, sizeof(dp));
+        if(s.front() == '0')
+            return 0;
+        
+        vector<int> dp(s.length()+1);
+        dp[0] = 1;
+        
+        for(int i=1; i<=s.length(); i++) {
+            if(s[i-1] != '0') 
+                dp[i] = dp[i-1];
+            if(i>=2) {
+                int x = (s[i - 2] - '0') * 10 + s[i - 1] - '0';
+                if (10 <= x && x <= 26) 
+                    dp[i] += dp[i - 2];
+            }
+        }
+        
+        return dp[s.length()];
     }
 };
