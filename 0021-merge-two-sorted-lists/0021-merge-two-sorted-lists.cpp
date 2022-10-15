e90daa885944217fb234f0028a50af59a3a8a2c2
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2022-10-14 20:50:59 -0400

    Time: 19 ms (24.05%), Space: 14.7 MB (97.84%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,42 +11,53 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(list1 == NULL)
+//             return list2;
+//         if(list2 == NULL)
+//             return list1;
+        
+//         ListNode* head = NULL; 
+        
+        
+//         if(list1->val <= list2->val) {
+//             head = list1;
+//             list1 = list1->next;
+//         }
+//         else {
+//             head = list2;
+//             list2 = head->next;
+//         }
+        
+//         ListNode* temp = head;
+//         while(list1 && list2) {
+//             if(list1->val <= list2->val) {
+//                 temp->next = list1;
+//                 list1 = list1->next;
+//                 temp = temp->next;
+//             } else {
+//                 temp->next = list2;
+//                 list2 = list2->next;
+//                 temp = temp->next;
+//             }
+            
+//         }
+        
+//         if(list1)
+//             temp->next = list1;
+//         if(list2)
+//             temp->next = list2;
+        
         if(list1 == NULL)
             return list2;
-        if(list2 == NULL)
+        else if(list2 == NULL)
             return list1;
-        
-        ListNode* head = NULL; 
-        
-        
-        if(list1->val <= list2->val) {
-            head = list1;
-            list1 = list1->next;
-        }
-        else {
-            head = list2;
-            list2 = head->next;
+        else if(list1->val <= list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
         }
         
-        ListNode* temp = head;
-        while(list1 && list2) {
-            if(list1->val <= list2->val) {
-                temp->next = list1;
-                list1 = list1->next;
-                temp = temp->next;
-            } else {
-                temp->next = list2;
-                list2 = list2->next;
-                temp = temp->next;
-            }
-            
-        }
-        
-        if(list1)
-            temp->next = list1;
-        if(list2)
-            temp->next = list2;
-        
-        return head;
     }
 };
