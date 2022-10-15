dfd8c3e733ae3a13c86bef6abd327606ef4a8de5
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2022-10-14 20:51:19 -0400

    Time: 8 ms (85.32%), Space: 14.8 MB (42.36%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,53 +11,54 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-//         if(list1 == NULL)
-//             return list2;
-//         if(list2 == NULL)
-//             return list1;
-        
-//         ListNode* head = NULL; 
-        
-        
-//         if(list1->val <= list2->val) {
-//             head = list1;
-//             list1 = list1->next;
-//         }
-//         else {
-//             head = list2;
-//             list2 = head->next;
-//         }
-        
-//         ListNode* temp = head;
-//         while(list1 && list2) {
-//             if(list1->val <= list2->val) {
-//                 temp->next = list1;
-//                 list1 = list1->next;
-//                 temp = temp->next;
-//             } else {
-//                 temp->next = list2;
-//                 list2 = list2->next;
-//                 temp = temp->next;
-//             }
-            
-//         }
-        
-//         if(list1)
-//             temp->next = list1;
-//         if(list2)
-//             temp->next = list2;
-        
         if(list1 == NULL)
             return list2;
-        else if(list2 == NULL)
+        if(list2 == NULL)
             return list1;
-        else if(list1->val <= list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        } else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+        
+        ListNode* head = NULL; 
+        
+        
+        if(list1->val <= list2->val) {
+            head = list1;
+            list1 = list1->next;
         }
+        else {
+            head = list2;
+            list2 = head->next;
+        }
+        
+        ListNode* temp = head;
+        while(list1 && list2) {
+            if(list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+                temp = temp->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+                temp = temp->next;
+            }
+            
+        }
+        
+        if(list1)
+            temp->next = list1;
+        if(list2)
+            temp->next = list2;
+        return head;
+        
+        // if(list1 == NULL)
+        //     return list2;
+        // else if(list2 == NULL)
+        //     return list1;
+        // else if(list1->val <= list2->val) {
+        //     list1->next = mergeTwoLists(list1->next, list2);
+        //     return list1;
+        // } else {
+        //     list2->next = mergeTwoLists(list1, list2->next);
+        //     return list2;
+        // }
         
     }
 };
