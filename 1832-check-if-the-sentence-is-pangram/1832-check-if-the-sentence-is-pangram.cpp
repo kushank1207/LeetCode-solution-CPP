eb9420d9a185d051ba4cad162f64648e54d027bb
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2022-10-17 19:53:45 -0400

    Time: 4 ms (65.73%), Space: 7.1 MB (6.62%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        if(sentence.length() < 26)
-            return false;
-        unordered_map<char, int> mp;
-        
-        for(char i:sentence) {
-            mp[i]++;
-        }
-        
-        if(mp.size() >= 26) {
-            return true;
-        }
-        else
-            return false;
+        unordered_set<char> mp(sentence.begin(), sentence.end());
+        return mp.size()==26;
     }
 };
