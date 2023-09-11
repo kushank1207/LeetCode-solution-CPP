051f7348a824a44bd4770a86f45bc38f0c9214b1
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2023-09-11 02:06:50 -0400

    Time: 0 ms (100.00%), Space: 6 MB (90.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -4,15 +4,15 @@ public:
         int dp[m][n];
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
-                if(j==n-1 || i==m-1)
+                if(j==0 || i==0)
                     dp[i][j] = 1;
             }
         }
-        for(int i=m-2; i>=0; i--) {
-            for(int j=n-2; j>=0; j--) {
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return dp[0][0];
+        return dp[m-1][n-1];
     }
 };
