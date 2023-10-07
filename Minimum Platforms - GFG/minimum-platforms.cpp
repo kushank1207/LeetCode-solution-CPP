d9cfb86b847e20824acadd791ccfb15c50db8f30
Kushank singh <63755171+kushank1207@users.noreply.github.com>
2023-10-07 01:24:48 -0400

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -7,35 +7,32 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-    struct timing {
-        int arrival;
-        int departure;
-    };
     public:
     //Function to find the minimum number of platforms required at the
     //railway station such that no train waits.
-
     int findPlatform(int arr[], int dep[], int n)
     {
     	// Your code here
-    	sort(arr,arr+n);
+        sort(arr,arr+n);
         sort(dep,dep+n);
-        
-    	int ans=1, count=1;
+    
+        int ans=1, count=1;
         int i=1,j=0;
-        while(i<n && j<n) {
-            if(arr[i]<=dep[j]) {
+        while(i<n && j<n)
+        {
+            if(arr[i]<=dep[j]) //one more platform needed
+            {
                 count++;
                 i++;
             }
-            else {
+            else //one platform can be reduced
+            {
                 count--;
                 j++;
             }
-            ans=max(ans,count);
+            ans=max(ans,count); //updating the value with the current maximum
         }
-        
-    	return ans;
+        return ans;    	
     }
 };
 
